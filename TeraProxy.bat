33996ae5a0e5fe79c6e007549cd2c2c2050cde8d
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-01-27 23:51:44 +0100

    Wrong formatting

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -6,5 +6,6 @@ WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
   PAUSE
+) ELSE (
+  START cmd.exe /k "node proxy.js"
 )
-ELSE START cmd.exe /k "node proxy.js"
