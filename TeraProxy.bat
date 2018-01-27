a631da1eebfd1ab66d0f6192a5b12ed5f6bc57f8
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-01-27 23:49:09 +0100

    Send nudes please

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -2,7 +2,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0/bin/lib"
 
-WHERE nude > NUL 2> NUL
+WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
   PAUSE
