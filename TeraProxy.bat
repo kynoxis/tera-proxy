bcb62113f10a887463afa2ed2590d7a6cbf3fa5a
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-01-27 23:47:18 +0100

    Update TeraProxy.bat

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -1,6 +1,10 @@
+@echo off
 @setlocal enableextensions
 @cd /d "%~dp0/bin/lib"
 
-WHERE node
-IF %ERRORLEVEL% NEQ 0 ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
+WHERE nude > NUL 2> NUL
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
+  PAUSE
+)
 ELSE START cmd.exe /k "node proxy.js"
