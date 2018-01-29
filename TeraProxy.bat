4e46028f16c10924dac9d7727224a2d013b46619
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-01-29 22:44:27 +0100

    Should also mention we don't support 32 bit

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -4,7 +4,7 @@
 
 WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
-  ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
+  ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest 64-bit version.
   PAUSE
 ) ELSE (
   START cmd.exe /k "node proxy.js"
