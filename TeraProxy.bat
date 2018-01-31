d261384e80d9f1cbc2cf5643a1c8475892062e81
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-01-31 17:24:32 +0100

    tera-server-select

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -1,10 +1,17 @@
 @echo off
 @setlocal enableextensions
-@cd /d "%~dp0/bin/lib"
+@cd /d "%~dp0/bin"
+IF NOT EXIST config.json (
+  ECHO Config file not found
+  ECHO Please select your region in the prompt and press OK
+  START /WAIT ..\tera-server-selector
+)
 
+
+@cd /d "%~dp0/bin/lib"
 WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
-  ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest 64-bit version.
+  ECHO Node.js is not installed. Please go to https://nodejs.org/ and install the latest stable version.
   PAUSE
 ) ELSE (
   START cmd.exe /k "node proxy.js"
