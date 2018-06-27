0261eb67251aa68711d77f9cd562d0ea9b377237
Pinkie Pie <yarnovax@gmail.com>
2018-06-27 11:12:12 -0400

    Update TeraProxy.bat

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -1,17 +1,28 @@
-@echo off
-@setlocal enableextensions
-@cd /d "%~dp0/bin"
-IF NOT EXIST config.json (
-  ECHO Please select your gameserver region in the prompt and press OK.
-  START /WAIT ..\server-select
-)
+@ECHO off
+TITLE TERA Proxy
+CD /d "%~dp0"
 
-
-@cd /d "%~dp0/bin/lib"
 WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
-  ECHO Node.JS not found. Please install the latest Current version from https://nodejs.org/
-  PAUSE
-) ELSE (
-  START cmd.exe /k "node proxy.js"
+	ECHO Node.JS not found.
+	ECHO Please install the latest Current from https://nodejs.org/
+	PAUSE
+	EXIT
 )
+
+node --harmony-bigint -e "" 2> NUL
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO Your version of Node.JS is outdated.
+	ECHO Please install the latest Current from https://nodejs.org/
+	PAUSE
+	EXIT
+)
+
+IF NOT EXIST ./bin/config.json (
+	ECHO Please select your gameserver region in the prompt and press OK.
+	START /WAIT server-select
+	CLS
+)
+
+node --harmony-bigint ./bin/lib/proxy.js
+PAUSE
